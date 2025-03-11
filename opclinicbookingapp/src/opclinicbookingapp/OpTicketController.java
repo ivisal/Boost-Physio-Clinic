7a0304d5c9a63eb9a8e61f27488675a127672add
ivisal <vn24aar@herts.ac.uk>
2025-03-11 14:48:05 +0000

    Tweaks to appointmentID, view appointment FN, rafactoring code #2

--- opclinicbookingapp/src/opclinicbookingapp/OpTicketController.java
+++ opclinicbookingapp/src/opclinicbookingapp/OpTicketController.java
@@ -12,6 +12,7 @@ public class OpTicketController {
     helpersFunction helpers = new helpersFunction();
     HashMap<String, HashMap<String, Integer>> appointments = new HashMap<>();
     String throwMessage = "Ooho!! Something error occured. Please try again.";
+    int initial = 0;
 
     // Fetches patient details and books an appointment
     private Patient bookAnAppointment() {
@@ -28,11 +29,11 @@ public class OpTicketController {
             HashMap<String, Integer> appointment = new HashMap<>();
             Patient patient = new Patient();
             Random random = new Random();
-            int uniqueID = random.nextInt(90) + 10; // Generates a random number between 10 and 99
-            while (!randomNumbersArray.contains(uniqueID)) // Ensure unique ID
-                randomNumbersArray.add(uniqueID);
+            int uniquePID = random.nextInt(90) + 10; // Generates a random number between 10 and 99
+            while (!randomNumbersArray.contains(uniquePID)) // Ensure unique ID
+                randomNumbersArray.add(uniquePID);
 
-            patient.setId(uniqueID);
+            patient.setId(uniquePID);
             System.out.println("Please enter patient name: ");
             patientName = inputString.nextLine();
             patient.setPatientName(patientName);
@@ -57,19 +58,20 @@ public class OpTicketController {
             patient.setPhone(phoneNumber);
 
             // Store the appointment with the unique key
-            appointment.put("physioid", physioId);
-            appointment.put("patientid", uniqueID);
+            appointment.put("appointmentID", opCount);
+            appointment.put("physioID", physioId);
+            appointment.put("patientID", uniquePID);
             String appointmentKey = "appointment" + opCount;
             appointments.put(appointmentKey, appointment);
 
             // Show stored appointments
             System.out.println("Current appointments: ");
             System.out.println(appointments);
-            printReceipt(patient, 30007, 200);
+            printReceipt(patient, physioId);
             return patient;
         } else {
             if(physioId == 0){
-                throwMessage = "Thankyou for visiting Boost Physio Clinic, have a nice day.";
+                throwMessage = "Thank you for visiting Boost Physio Clinic, have a nice day.";
                 helpers.throwError(throwMessage);
                 System.exit(1);
                 return null;
@@ -89,7 +91,7 @@ public class OpTicketController {
             System.out.print("1. Book an appointment\t\t");
             System.out.println("2. Exit");
         }else{
-            System.out.print("1. Book an appointment\t\t");
+            System.out.print("1. Book another appointment\t\t");
             System.out.print("2. View appointment\t\t");
             System.out.print("3. Cancel appointment\t\t");
             System.out.println("4. Exit");
@@ -105,7 +107,14 @@ public class OpTicketController {
         physio.showAvailablePhysios();
     }
 
-    public void printReceipt(Patient patient, int physioId, int appId) {
+    public void printReceipt(Patient patient, int physioId) {
+        try {
+            System.out.println("Your receipt is printing...");
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
         HashMap<String, String> bookedPhysio = helpers.getPhysioByKey(physioId);
         System.out.println();
         System.out.println("Boost Physio Clinic, United Kingdom");
@@ -114,14 +123,14 @@ public class OpTicketController {
         System.out.println("Booking Receipt");
         System.out.println("-----------------");
         System.out.println();
-        System.out.println("Appointment ID: " + appId);
+        System.out.println("Appointment ID: " + patient.getId());
         System.out.println("Patient Name: " + patient.getPatientName());
         System.out.println("Physician: " + bookedPhysio.get("Name").toUpperCase());
         System.out.println("Expertise: " + bookedPhysio.get("Expertise"));
         System.out.println("Booking Slot: " + bookedPhysio.get("Available Time"));
         System.out.println();
         System.out.println("Thankyou for booking with Boost Physio Clinic, have a nice day.");
-        System.exit(1);
+
     }
     public void typeOfAppointment() {
         System.out.println();
@@ -147,6 +156,7 @@ public class OpTicketController {
                         System.out.println("Expertise: " + searchedPhysio.get("Expertise"));
                         System.out.println("Avaliable From: " + searchedPhysio.get("Available Time"));
                         bookAnAppointment();
+                        break;
                     }else{
                         throwMessage = "No Physio found!";
                         helpers.throwError(throwMessage);
@@ -157,6 +167,29 @@ public class OpTicketController {
                 }
             case 2:
                 listAvailablePhysio(); // List available physiotherapists
+                break;
+        }
+    }
+
+    public void viewAppointments( HashMap<String, HashMap<String, Integer>> appointments  ){
+        System.out.println();
+        System.out.println("1. View Your appointment by appointment ID ");
+        System.out.println("2. View all your appointments ");
+        System.out.print("Choose a option: ");
+        switch (input.nextInt()){
+            case 1:
+                System.out.print("Enter your appointment ID: ");
+                int appointmentID = input.nextInt();
+                if(appointmentID > 0 ){
+                    System.out.print("-----------");
+                    break;
+                }
+            case 2:
+                System.out.println("Your appointments: ");
+                break;
+            default:
+                helpers.throwError(throwMessage);
+                break;
         }
     }
 
@@ -166,26 +199,44 @@ public class OpTicketController {
     public void performUserActions() {
         boolean running = true;
         while (running) {
-            listUserActions(0);
-            switch (input.nextInt()) {
-                case 1:
-                    typeOfAppointment();
-                    bookAnAppointment(); // Book an appointment
-                    break;
-                case 2:
-                    System.out.println("View appointment");
-                    break;
-                case 3:
-                    System.out.println("Cancel appointment");
-                    break;
-                case 4:
-                    System.out.println("Exit");
-                    running = false;
-                    break;
-                default:
-                    helpers.throwError(throwMessage); // Handle invalid input
-                    break;
+            listUserActions(initial);
+            if(initial==0){
+                switch (input.nextInt()) {
+                    case 1:
+                        typeOfAppointment();
+                        bookAnAppointment(); // Book an appointment
+                        break;
+                    case 2:
+                        System.out.println("Exit");
+                        running = false;
+                        break;
+                    default:
+                        helpers.throwError(throwMessage); // Handle invalid input
+                        break;
+                }
+            }else{
+                switch (input.nextInt()) {
+                    case 1:
+                        typeOfAppointment();
+                        bookAnAppointment();
+                        break;
+                    case 2:
+                        viewAppointments(appointments);
+                        break;
+                    case 3:
+                        System.out.println("Cancel appointment");
+                        break;
+                    case 4:
+                        System.out.println("Exit");
+                        running = false;
+                        break;
+                    default:
+                        helpers.throwError(throwMessage); // Handle invalid input
+                        break;
+                }
             }
+
+            initial++;
         }
     }
 
